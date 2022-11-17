3e54a08f3caef670de8105638b8d7499abde98c9
Digvijay Phutane <digvijay.phutane@gmail.com>
2022-11-17 20:05:01 +0530

    Create pymath.py

---
+++ pymath.py
@@ -0,0 +1,19 @@
+import math
+
+x = math.sqrt(64)
+a = math.factorial(x)
+p = math.pi
+print(x)
+print(a)
+print(p)
+c = pow(4, 3)
+
+print(c)
+b = abs(-7.25)
+
+print(b)
+n = min(5, 10, 25)
+m = max(5, 10, 25)
+
+print(n)
+print(m)
